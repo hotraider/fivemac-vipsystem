5b71adb1a9a34cfcc0e9d591b741d606dfbcd43c
hotraider <92651180+hotraider@users.noreply.github.com>
2021-12-01 00:25:34 +0300

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,84 +1,56 @@
-fivemac = {}
+ fivemac = {}
+ $(function() {
+     function display(bool) {
+         if (bool) {
+             $("#container").show();
+         } else {
+             $("#container").hide();
+         }
+     }
+     display(false)
+     window.addEventListener('message', function(event) {
+         var item = event.data;
+         if (item.type === "ui") {
+             if (item.status == true) {
+                 display(true)
+             } else {
+                 display(false)
+             }
+         }
+     })
+     $("#yazi").click(function() {
+         let inputValue = $("#yazi").val()
+         return;
+     })
+ })
 
-$(function() {
-    function display(bool) {
-        if (bool) {
-            $("#container").show();
-        } else {
-            $("#container").hide();
-        }
-    }
+ $(document).on('keydown', function() {
+     switch (event.keyCode) {
+         case 27:
+             fivemac.Close();
+             break;
+     }
+ });
 
-    display(false)
-
-    window.addEventListener('message', function(event) {
-        var item = event.data;
-        if (item.type === "ui") {
-            if (item.status == true) {
-                display(true)
-            } else {
-                display(false)
-            }
-        }
-    })
-
-    $("#yazi").click(function() {
-        let inputValue = $("#yazi").val()
-        return;
-    })
-})
-
-$(document).on('keydown', function() {
-    switch (event.keyCode) {
-        case 27:
-            fivemac.Close();
-            break;
-    }
-});
-
-fivemac.Close = function() {
-    $("#container").fadeOut(175);
-    $.post('http://fivemac-vipsystem/close');
-}
+ fivemac.Close = function() {
+     $("#container").fadeOut(175);
+     $.post('http://fivemac-vipsystem/close');
+ }
 
 
-$("#exit").click(function() {
-    fivemac.Close();
-
-});
-
-let selected = 0;
+ $("#exit").click(function() {
+     fivemac.Close();
+ });
 
 
-
-$("#paket1").click(function() {
-    $.post('http://fivemac-vipsystem/weapon', JSON.stringify({ id: selected, type: 'paket1' }));
-    $(this).blur()
-})
-
-$("#paket2").click(function() {
-    $.post('http://fivemac-vipsystem/weapon', JSON.stringify({ id: selected, type: 'paket2' }));
-    $(this).blur()
-})
-
-$("#paket3").click(function() {
-    $.post('http://fivemac-vipsystem/weapon', JSON.stringify({ id: selected, type: 'paket3' }));
-    $(this).blur()
-})
+ $(".plan").on("click", function() {
+     let price = $(this).attr('data-id');
+     let type = $(this).attr('id');
 
 
+     $.post("http://fivemac-vipsystem/weapon", JSON.stringify({
+         price: price,
+         type: type,
 
-
-
-
-$('header span').on('click', function() {
-    if ($(this).index() == 1) {
-        $('.plan').css({
-            fontSize: '+=2'
-        });
-    } else {
-        $('.plan').css({
-            fontSize: '-=2'
-        });
-    }
-});
+     }));
+ })
