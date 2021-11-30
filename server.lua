65f3398cb1c9ef13001221cc59933bc72cbb5c27
hotraider <92651180+hotraider@users.noreply.github.com>
2021-12-01 00:24:27 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,15 +4,18 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 
 RegisterServerEvent('server:tick')
-AddEventHandler('server:tick', function(type)
+AddEventHandler('server:tick', function(type,para)
     local player = ESX.GetPlayerFromId(source)
     local source = source
     local item = player.getQuantity('doge')
-    
-    if item > 0 then
-     for i, v in pairs(Config.Paketler[type]) do
-       player.addInventoryItem(v, 1)
-     end
-     player.removeInventoryItem('doge', 1)
-    end
+
+
+    if player.getQuantity('doge') >= para then
+        for i, v in pairs(Config.Paketler[type]) do
+          player.addInventoryItem(v, 1)
+        end
+        player.removeInventoryItem('doge', para)
+      else 
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Yetersiz Coin.' } )
+      end 
 end)
