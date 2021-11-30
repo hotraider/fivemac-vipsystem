02c9cefa174b9cef8903d664e6db771f960fa3dd
hotraider <92651180+hotraider@users.noreply.github.com>
2021-11-30 16:54:32 +0300

    server.lua
    Credit:@S4LEH

--- server.lua
+++ server.lua
@@ -6,40 +6,13 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('server:tick')
 AddEventHandler('server:tick', function(type)
     local player = ESX.GetPlayerFromId(source)
-	local source = source
+    local source = source
     local item = player.getQuantity('doge')
-	
-    if item > 0 then
-
-	if type == "paket1" then
-
-    for i=4,1,-1 do 
-        
-    player.addInventoryItem(Config.Paket1[i], 1)
-
-    player.removeInventoryItem('doge', 1)
     
+    if item > 0 then
+     for i, v in pairs(Config.Paketler[type]) do
+       player.addInventoryItem(v, 1)
+     end
+     player.removeInventoryItem('doge', 1)
     end
-
-    elseif type == "paket2" then
-        
-    for i=4,1,-1 do 
-
-    player.addInventoryItem(Config.Paket2[i], 1)
-
-    player.removeInventoryItem('doge', 1)
-
-    end
-
-    elseif type == "paket3" then
-
-    for i=4,1,-1 do 
-
-    player.addInventoryItem(Config.Paket3[i], 1)
-
-    player.removeInventoryItem('doge', 1)
-    end 
-    end
-    end
-    end)
-
+end)
