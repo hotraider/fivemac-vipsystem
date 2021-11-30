40e8d22df2aa0d123f74f97a491e004a8f27ff9a
hotraider <92651180+hotraider@users.noreply.github.com>
2021-11-30 16:55:06 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,23 +1,25 @@
 Config = {}
 
-Config.Paket1 = {
+
+Config.Paketler = {
+
+  paket1 = {
     [1] = "weapon_pistol",
     [2] = "weapon_assaultrifle",
     [3] = "weapon_knife",
     [4] = "weapon_smg",
-}
-
-
-Config.Paket2 = {
+  },
+  paket2 = {
     [1] = "weapon_battleaxe",
     [2] = "weapon_bat",
     [3] = "weapon_knuckle",
     [4] = "weapon_flashlight",
-}
-
-Config.Paket3 = {
+  },
+  paket3 = {
     [1] = "weapon_revolver",
     [2] = "weapon_pistol50",
     [3] = "weapon_machinepistol",
     [4] = "weapon_battleaxe",
+  }
+  
 }
